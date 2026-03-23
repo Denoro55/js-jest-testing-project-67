8baf0da098be77cbc36407bbb572c814e3af4c51
Denis <denis.chertenko@mail.ru>
2026-03-24 00:31:05 +0500

    fix

--- __tests__/getResourceName.test.js
+++ __tests__/getResourceName.test.js
@@ -1,13 +1,13 @@
-import { jest } from '@jest/globals'
+import { test, expect } from '@jest/globals'
 
 console.info('===> test 1')
 
 const sum = (a, b) => a + b
 
-jest.test('should return the sum of two numbers', () => {
+test('should return the sum of two numbers', () => {
   console.info('===> test 2')
 
-  jest.expect(sum(1, 2)).toBe(3)
+  expect(sum(1, 2)).toBe(3)
 
   console.info('===> test 3')
 })
