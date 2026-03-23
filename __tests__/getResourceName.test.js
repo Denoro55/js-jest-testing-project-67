89760f263b3da9c8797304e315ad9b9b00419792
Denis <denis.chertenko@mail.ru>
2026-03-23 23:35:40 +0500

    logs

--- __tests__/getResourceName.test.js
+++ __tests__/getResourceName.test.js
@@ -1,5 +1,11 @@
+console.info('===> test 1');
+
 describe('getResourceName', () => {
+    console.info('===> test 2');
+
     it('should return the name of the resource', () => {
+        console.info('===> test 3');
+
         expect(true).toBe(true);
     });
 });
