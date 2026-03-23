d8618055b5ee15306b55396880db718fcd7e55e4
Denis <denis.chertenko@mail.ru>
2026-03-23 12:34:19 +0500

    js test

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ export default {
         '^(\\.{1,2}/.*)\\.js$': '$1',  // убирает .js для импортов в тестах (опционально)
     },
     // Тестовые файлы
-    testMatch: ['**/__tests__/**/*.test.ts'],
+    testMatch: ['**/__tests__/**/*.test.js'],
     // Генерация покрытия кода
     collectCoverage: false,
     coverageDirectory: 'coverage', // папка для отчётов
