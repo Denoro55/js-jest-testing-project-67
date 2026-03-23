ad426ac22ea2a87a82911504b404f2b183bd3851
Denis <denis.chertenko@mail.ru>
2026-03-23 12:39:36 +0500

    test

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 /** @type {import('jest').Config} */
 export default {
     testEnvironment: 'node',
-    moduleNameMapper: {
-        '^(\\.{1,2}/.*)\\.js$': '$1',  // убирает .js для импортов в тестах (опционально)
-    },
     // Тестовые файлы
     testMatch: ['**/__tests__/**/*.test.js'],
     // Генерация покрытия кода
     collectCoverage: false,
     coverageDirectory: 'coverage', // папка для отчётов
     coverageReporters: ['json', 'lcov', 'text', 'clover'],
-
+  
     // Какие файлы включать в покрытие
-    collectCoverageFrom: ['src/**/*.ts']
-};
+    collectCoverageFrom: [
+      'src/**/*.js',
+    ],
+    transform: {},
+  }
