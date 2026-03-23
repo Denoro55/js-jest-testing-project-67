00e1e41fea3e7c1f4a41d7fe3ccd11fafd18cc67
Denis <denis.chertenko@mail.ru>
2026-03-23 12:20:15 +0500

    check without test

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,8 @@
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-
 /** @type {import('jest').Config} */
 export default {
     testEnvironment: 'node',
     moduleNameMapper: {
         '^(\\.{1,2}/.*)\\.js$': '$1',  // убирает .js для импортов в тестах (опционально)
-        '^axios$': require.resolve('axios'),
     },
     // Тестовые файлы
     testMatch: ['**/__tests__/**/*.test.ts'],
